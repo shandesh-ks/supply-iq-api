5a93ee41bfa50c81d1273665085068086f317201
Shandesh <shandesh@experience.com>
2025-03-28 12:40:23 +0530

    update start.sh

--- start.sh
+++ start.sh
@@ -7,9 +7,11 @@ then
     apt-get update && apt-get install -y python3 python3-pip
 fi
 
+# Upgrade pip
+pip3 install --upgrade pip
+
 # Install dependencies globally
-pip3 install --user --upgrade pip
-pip3 install --user -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt
 
 # Start the Rails server
 bundle exec rails server -b 0.0.0.0 -p $PORT
